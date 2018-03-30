42023f34d53dde00fe99a6a8616632e55731d757
JMartins27396 <36698360+JMartins27396@users.noreply.github.com>
2018-03-30 18:05:03 +0100

    Correçoes e script
    Adicionado Script sql com a criação das tabelas e correçoes de tipos de dados nas relações.

---
+++ Fase2/script.sql
@@ -0,0 +1,76 @@
+
+
+CREATE TABLE "Construtor"(
+  "Nome" VARCHAR2(30 CHAR) PRIMARY KEY,
+  "Construção Amadora" VARCHAR(3 CHAR)
+) TAVLESPACE JMsDB;
+
+
+CREATE TABLE "Aeroporto "(
+  "Nome" VARCHAR2(30 CHAR) PRIMARY KEY,
+  "Pais" VARCHAR2(30 CHAR) PRIMARY KEY,
+  "Região" VARCHAR2(30 CHAR) PRIMARY KEY,
+  "Estado" VARCHAR2(25 CHAR) PRIMARY KEY,
+  "Código Aeroporto" VARCHAR(25 CHAR)
+) TAVLESPACE JMsDB;
+
+
+CREATE TABLE "Acidente "(
+  "Tipo de Investigação" VARCHAR2(10 CHAR) PRIMARY KEY,
+  "Número do Acidente" VARCHAR2(15 CHAR) PRIMARY KEY,
+  "Latitude" NUMBER(8,6),
+  "Longitude" NUMBER(9,6),
+  "Fase de Voo" VARCHAR2(15 CHAR),
+  "Data do Acidente" DATE,
+  "Dano Causado" VARCHAR2(13 CHAR),
+  "Severidade do Acidente" VARCHAR2(13 CHAR),
+  "Condições Atmosféricas" VARCHAR2(3 CHAR),
+  "Total de Feridos Fatais" NUMBER(2),
+  "Total de Feridos Sérios" NUMBER(2),
+  "Total de Feridos Menores" NUMBER(2),
+  "Total de Feridos Ilesos" NUMBER(3)
+) TAVLESPACE JMsDB;
+
+
+CREATE TABLE "Investigador "(
+"Investigador" VARCHAR2(50 CHAR) PRIMARY KEY,
+"Crachá" VARCHAR2(10 CHAR)
+"Morada" VARCHAR2(100 CHAR)
+) TAVLESPACE JMsDB;
+
+CREATE TABLE "Transportadora "(
+"Transportadora" VARCHAR2(20) PRIMARY KEY,
+"Código" VARCHAR2(5 CHAR),
+"Demonização Completa" VARCHAR2(20 CHAR),
+"Pais" VARCHAR2(15 CHAR)
+) TAVLESPACE JMsDB;
+
+CREATE TABLE "Investigação "(
+  "Investigador" VARCHAR2(50 CHAR) FOREIGN KEY REFERENCES "Investigador"("Investigador"),
+  "Tipo de Investigação" VARCHAR2(10 CHAR) FOREIGN KEY REFERENCES "Acidente"("Tipo de Investigação"),
+  "Número do Acidente" VARCHAR2(15 CHAR) FOREIGN KEY REFERENCES "Acidente"("Número de Acidente"),
+  "Data de Inicio" DATE,
+  "Data de Fim" DATE,
+  "Estado" VARCHAR2(15 CHAR)
+) TAVLESPACE JMsDB;
+
+
+CREATE TABLE "Voo "(
+  "Identificador Aeronave" VARCHAR2(30 CHAR) FOREIGN KEY REFERENCES "Aeronave"("Registo"),
+  "Nome Aeroporto" VARCHAR2(30 CHAR) FOREIGN KEY REFERENCES "Aeroporto"("Nome"),
+  "Pais" VARCHAR2(30 CHAR) FOREIGN KEY REFERENCES "Aeroporto"("Pais"),
+  "Região" VARCHAR2(30 CHAR) FOREIGN KEY REFERENCES "Aeroporto"("Região"),
+  "Estado" VARCHAR2(30 CHAR) FOREIGN KEY REFERENCES "Aeroporto"("Estado"),
+  "Transportadora Aérea"  VARCHAR2(30 CHAR) FOREIGN KEY REFERENCES "Transportadora"("Transportadora"),
+  "Tipo de Voo" VARCHAR2(30 CHAR),
+  "Propósito de Voo" VARCHAR2(30 CHAR)
+) TAVLESPACE JMsDB;
+
+CREATE TABLE "Aeronave "(
+  "Registo" VARCHAR2(6 CHAR) PRIMARY KEY,
+  "Nome Construtor" VARCHAR2(30 CHAR) FOREIGN KEY REFERENCES "Construtor"("Nome"),
+  "Categoria" VARCHAR2(25 CHAR),
+  "Modelo" VARCHAR2(25 CHAR),
+  "Tipo de Motor" VARCHAR(25 CHAR),
+  "Numero de Motores" NUMBER
+) TAVLESPACE JMsDB;
